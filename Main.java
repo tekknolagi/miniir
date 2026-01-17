a7ead02a8dcb87335c2f61654e2ea7b398d0c6e8
Max Bernstein <max@bernsteinbear.com>
2026-01-17 11:11:58 -0500

    Value numbering
    Squashed commit of the following:
    
    commit b48b2e9ff23341150f139a770e81abf7999c13c8
    Author: Max Bernstein <max@bernsteinbear.com>
    Date:   Sat Jan 17 11:11:40 2026 -0500
    
        .
    
    commit fbcffdfeb54535800e8ff3ee417093d496837ddd
    Author: Max Bernstein <ruby@bernsteinbear.com>
    Date:   Fri Jan 16 23:37:48 2026 -0500
    
        .

--- Main.java
+++ Main.java
@@ -2,13 +2,14 @@ import java.util.Iterator;
 import java.util.Collections;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Arrays;
 
 class Insn {
   Insn(Insn... operands) { this.operands = operands; this.subst = this; }
   Insn[] operands() { return operands; }
-  long valueNumber() { return 0; }
-  long defaultValueNumber() {
-    long result = System.identityHashCode(this.getClass());
+  int valueNumber() { return 0; }
+  int defaultValueNumber() {
+    int result = System.identityHashCode(this.getClass());
     for (Insn operand : operands) {
       result <<= 8;
       result |= System.identityHashCode(operand);
@@ -21,7 +22,7 @@ class Insn {
     if (this.getClass() != other.getClass()) {
       return false;
     }
-    return this.operands() == other.operands();
+    return Arrays.equals(operands, other.operands());
   }
   String immediate() { return ""; }
   Insn find() {
@@ -39,10 +40,11 @@ class Insn {
     }
     return root2;
   }
-  void apply() {
+  Insn apply() {
     for (int i = 0; i < operands.length; i++) {
       operands[i] = operands[i].find();
     }
+    return this;
   }
 
   protected Insn[] operands = null;
@@ -53,7 +55,7 @@ class Const extends Insn {
   public Const(int value) { this.value = value; }
   public int value() { return value; }
   @Override
-  long valueNumber() { return defaultValueNumber(); }
+  int valueNumber() { return defaultValueNumber(); }
   @Override
   boolean valueEqual(Insn other) {
     if (!(other instanceof Const)) {
@@ -71,7 +73,7 @@ class Const extends Insn {
 class Add extends Insn {
   public Add(Insn left, Insn right) { super(left, right); }
   @Override
-  long valueNumber() { return defaultValueNumber(); }
+  int valueNumber() { return defaultValueNumber(); }
   @Override
   boolean valueEqual(Insn other) { return defaultValueEqual(other); }
 }
@@ -83,6 +85,7 @@ class Return extends Insn {
 class Block {
   Block() { insns = new ArrayList<>(); }
   Insn append(Insn insn) { insns.add(insn); return insn; }
+  void replaceInsns(ArrayList<Insn> new_insns) { insns = new_insns; }
   void print() {
     int next_id = 0;
     HashMap<Insn, String> names = new HashMap<>();
@@ -114,13 +117,150 @@ class Block {
   protected ArrayList<Insn> insns;
 }
 
+/* BEGIN From Maxine VM */
+/* Copyright (c) 2009, 2011, Oracle and/or its affiliates. GPLv2 licensed. */
+class ValueMap {
+    private static class Link {
+        final ValueMap map;
+        final int valueNumber;
+        final Insn value;
+        final Link next;
+
+        Link(ValueMap map, int valueNumber, Insn value, Link next) {
+            this.map = map;
+            this.valueNumber = valueNumber;
+            this.value = value;
+            this.next = next;
+        }
+    }
+
+    private final ValueMap parent;
+    private Link[] table;
+    private int count;
+    private int max;
+
+    public ValueMap() {
+        parent = null;
+        table = new Link[19];
+    }
+
+    public ValueMap(ValueMap parent) {
+        this.parent = parent;
+        this.table = parent.table.clone();
+        this.count = parent.count;
+        this.max = table.length + table.length / 2;
+    }
+
+    public Insn findInsert(Insn x) {
+        int valueNumber = x.valueNumber();
+        if (valueNumber != 0) {
+            // value number != 0 means the instruction can be value numbered
+            int index = indexOf(valueNumber, table);
+            Link l = table[index];
+            // hash and linear search
+            while (l != null) {
+                if (l.valueNumber == valueNumber && l.value.valueEqual(x)) {
+                    return l.value;
+                }
+                l = l.next;
+            }
+            // not found; insert
+            table[index] = new Link(this, valueNumber, x, table[index]);
+            if (count > max) {
+                resize();
+            }
+        }
+        return x;
+    }
+
+    public void killAll() {
+        assert parent == null : "should only be used for local value numbering";
+        for (int i = 0; i < table.length; i++) {
+            table[i] = null;
+        }
+        count = 0;
+    }
+
+    private void resize() {
+        Link[] ntable = new Link[table.length * 3 + 4];
+        if (parent != null) {
+            // first add all the parent's entries by cloning them
+            for (int i = 0; i < table.length; i++) {
+                Link l = table[i];
+                while (l != null && l.map == this) {
+                    l = l.next; // skip entries in this map
+                }
+                while (l != null) {
+                    // copy entries from parent
+                    int index = indexOf(l.valueNumber, ntable);
+                    ntable[index] = new Link(l.map, l.valueNumber, l.value, ntable[index]);
+                    l = l.next;
+                }
+            }
+        }
+
+        for (int i = 0; i < table.length; i++) {
+            Link l = table[i];
+            // now add all the entries from this map
+            while (l != null && l.map == this) {
+                int index = indexOf(l.valueNumber, ntable);
+                ntable[index] = new Link(l.map, l.valueNumber, l.value, ntable[index]);
+                l = l.next;
+            }
+        }
+        table = ntable;
+        max = table.length + table.length / 2;
+    }
+
+    private int indexOf(int valueNumber, Link[] t) {
+        return (valueNumber & 0x7fffffff) % t.length;
+    }
+}
+/* END from Maxine VM */
+
+class Function {
+  Function() { blocks = new ArrayList<>(); }
+
+  void addBlock(Block block) {
+    blocks.add(block);
+  }
+
+  void localValueNumbering() {
+    for (Block block : blocks) {
+      ValueMap vmap = new ValueMap();
+      ArrayList<Insn> new_insns = new ArrayList<>();
+      for (Insn insn : block.insns) {
+        Insn found = insn.find().apply();
+        Insn valueNumbered = vmap.findInsert(found);
+        if (valueNumbered != found) {
+          found.replace(valueNumbered);
+        } else {
+          new_insns.add(found);
+        }
+      }
+      block.replaceInsns(new_insns);
+    }
+  }
+
+  protected ArrayList<Block> blocks;
+}
+
 class Main {
     public static void main(String[] args) {
       Block block = new Block();
       Insn left = block.append(new Const(1));
       Insn right = block.append(new Const(2));
       Insn add = block.append(new Add(left, right));
-      Insn ret = block.append(new Return(add));
+      Insn left1 = block.append(new Const(1));
+      Insn right1 = block.append(new Const(2));
+      Insn add1 = block.append(new Add(left, right));
+      Insn ret = block.append(new Return(add1));
+      Function function = new Function();
+      function.addBlock(block);
+      System.out.println("Before LVN:");
+      block.print();
+      function.localValueNumbering();
+      System.out.println("After LVN:");
       block.print();
     }
 }
